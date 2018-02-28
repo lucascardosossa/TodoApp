e05bf71c369a52e08a073c6ca72d827dd2339c1e
Lucas Cardoso da Silva <lucas.silva@ip3.info>
2018-02-28 14:29:47 -0300

    enviando arquivos iniciais

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1>{{ titulo }}</h1>
+    <h2>{{ subTitulo }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      titulo: 'Bem vindo ao Todo VUE',
+      subTitulo: 'Seu gerenciador de Tarefas - Simples e Efetivo'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
