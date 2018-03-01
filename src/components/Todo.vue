885657e4294ff1c9d7fb735e2298bd7d67d088fe
Lucas Cardoso da Silva <lucascardoso@MacBook-Air-Lucas.local>
2018-02-28 23:34:49 -0300

    Componente TODO

---
+++ src/components/Todo.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class='ui centered card'>
+    <div class='content'>
+        <div class='header'>
+            {{ todo.title }}
+        </div>
+        <div class='meta'>
+            {{ todo.project }}
+        </div>
+        <div class='extra content'>
+            <span class='right floated edit icon'>
+            <i class='edit icon'></i>
+          </span>
+        </div>
+    </div>
+    <div class='ui bottom attached green basic button' v-show="todo.done">
+        Completed
+    </div>
+    <div class='ui bottom attached red basic button' v-show="!todo.done">
+        Complete
+    </div>
+</div>
+</template>
+
+<script type="text/javascript">
+  export default {
+	name: 'Todo',
+	data() {
+		return {
+			todos:[
+				{
+                    id: 1,
+					title: 'Aprender REACT',
+					project: 'PWA',
+					done: true,
+				},
+				{
+                    id: 2,
+					title: 'Aprender VUE',
+					project: 'PWA',
+					done: false,
+				},
+				{
+                    id: 3,
+					title: 'Aprender VUEX',
+					project: 'PWA',
+					done: false,
+				}
+			],
+		};
+	},
+};
+</script>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,88 +1,31 @@
 <template>
-  <div>
+  
     <div class="ui three column stackable grid">
     	<div class="column">
     		<h3>Pendentes ({{todos.filter(todo => {return todo.done === false}).length}})</h3>
-    		<div class='ui centered card' v-for="todo in todos.filter(todo => {return todo.done === false})">
-			      <div class='content'>
-			        <div class='header'>
-			          {{ todo.title }}
-			        </div>
-			        <div class='meta'>
-			          {{ todo.project }}
-			        </div>
-			        <div class='extra content'>
-			          <span class='right floated edit icon'>
-			            <i class='edit icon'></i>
-			          </span>
-			        </div>
-			      </div>
-			      <div class='ui bottom attached green basic button' v-show="todo.done">
-			        Completed
-			      </div>
-			      <div class='ui bottom attached red basic button' v-show="!todo.done">
-			        Complete
-			      </div>
-			  </div>
-			</div>		
+				<todo  v-for="todo in todos.filter(todo => {return todo.done === false})" :key="todo.id" v-bind:todo="todo"></todo>
+    	</div>		
     	<div class="column">
     		<h3>Em Andamento (0)</h3>
     	</div>
 		<div class="column">
     		<h3>Finalizadas ({{todos.filter(todo => {return todo.done === true}).length}})</h3>
-    		<div class='ui centered card' v-for="todo in todos.filter(todo => {return todo.done === true})">
-			      <div class='content'>
-			        <div class='header'>
-			          {{ todo.title }}
-			        </div>
-			        <div class='meta'>
-			          {{ todo.project }}
-			        </div>
-			        <div class='extra content'>
-			          <span class='right floated edit icon'>
-			            <i class='edit icon'></i>
-			          </span>
-			        </div>
-			      </div>
-			      <div class='ui bottom attached green basic button' v-show="todo.done">
-			        Completed
-			      </div>
-			      <div class='ui bottom attached red basic button' v-show="!todo.done">
-			        Complete
-			      </div>
-			  </div>
-    		</div>
+    		<todo  v-for="todo in todos.filter(todo => {return todo.done === true})" :key="todo.id" v-bind:todo="todo"></todo>
     	</div>
     </div>
     
 </template>
 
 <script type = "text/javascript" >
+import Todo from './Todo';
 
 export default {
-	name: 'TodoList',
-	data() {
-		return {
-			todos:[
-				{
-					title: 'Aprender REACT',
-					project: 'PWA',
-					done: true,
-				},
-				{
-					title: 'Aprender VUE',
-					project: 'PWA',
-					done: false,
-				},
-				{
-					title: 'Aprender VUEX',
-					project: 'PWA',
-					done: false,
-				}
-			],
-		};
-	},
+  props: ['todos'],
+  components: {
+    Todo,
+  },
 };
+
 </script>
 
 <style>
